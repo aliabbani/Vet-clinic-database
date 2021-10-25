3ae71061f56e6478090ed3045e158d4ffa6a9c4a
ali abbani <aliox@ahlam.com>
2021-10-26 00:12:43 +0300

    Fix queries for finding all animal whose name end with mon

--- queries.sql
+++ queries.sql
@@ -1,9 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
-
 /* Find all animals whose name ends in "mon". */
-SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT * FROM animals WHERE name LIKE '%mon';
 /* List the name of all animals born between 2016 and 2019. */
 SELECT name FROM animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 2016 AND 2020;
 /* List the name of all animals that are neutered and have less than 3 escape attempts. */
