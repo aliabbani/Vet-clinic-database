8aec95f207e58a5778ad9cfe5c18c327fcd1223c
ali abbani <aliox@ahlam.com>
2021-10-26 11:21:45 +0300

    Remove spaces from my table

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-  id                INT GENERATED ALWAYS AS IDENTITY,
-  name              CHAR(30),
-  date_of_birth     DATE,
-  escape_attempts   INT,
-  neutered          BOOLEAN,
-  weight_kg         DECIMAL,
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(30) NOT NULL,
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL,
   PRIMARY KEY(id)
 );
