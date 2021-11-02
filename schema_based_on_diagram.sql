d30e90a5c7da57f3874e394ada0cd28faf1880b6
HENRY-KC <henrykc24@yahoo.com>
2021-11-02 18:10:45 +0100

    Add the reamaining tables to the database

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -32,3 +32,31 @@ CREATE TABLE treatments (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE invoice_items (
+  id INT SERIAL,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  PRIMARY KEY (id),
+  CONSTRAINT FK_invoice_items.invoice_id
+    FOREIGN KEY (invoice_id)
+      REFERENCES invoices(id),
+  CONSTRAINT FK_invoice_items.treatment_id
+    FOREIGN KEY (treatment_id)
+      REFERENCES treatments(id)
+);
+
+CREATE TABLE history_treatment_connection (
+  id  INT SERIAL,
+  treatment_id INT,
+  history_id INT,
+  PRIMARY KEY (id ),
+  CONSTRAINT FK_history_treatment_connection.treatment_id
+    FOREIGN KEY (treatment_id)
+      REFERENCES treatments(id),
+  CONSTRAINT FK_history_treatment_connection.history_id
+    FOREIGN KEY (history_id)
+      REFERENCES medical_histories(id)
+);
